574837936d46b1cb4cf5f753fff51ae9faed9a20
PrasannaJ333 <jprasanna734@gmail.com>
2021-10-29 10:53:27 +0530

    gateway api committed

---
+++ src/main/java/com/tourism/TourismGatewayApiApplication.java
@@ -0,0 +1,18 @@
+package com.tourism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class TourismGatewayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourismGatewayApiApplication.class, args);
+	}
+	
+
+}
